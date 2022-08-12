b32f0f276193ecb77c819d4eaa0f14e1c8276df7
andrewshan <andrewshan@tencent.com>
2022-08-12 10:45:44 +0800

    feat: 优化统计日志输出格式

--- polaris-sync-reporter-plugins/reporter-file/src/main/java/cn/polarismesh/polaris/sync/reporter/plugins/file/FileReportHandler.java
+++ polaris-sync-reporter-plugins/reporter-file/src/main/java/cn/polarismesh/polaris/sync/reporter/plugins/file/FileReportHandler.java
@@ -56,7 +56,7 @@ public class FileReportHandler implements ReportHandler {
         } else {
             reportInfoStr.append("\n");
             reportInfoStr.append(String
-                    .format("%-48s|%12s|%12s|%12s|%12s|\n", "", "Type", "Product", "Total", "Error"));
+                    .format("%-48s|%12s|%12s|%12s|%12s|\n", "Name", "Type", "Detail", "Total", "Error"));
             for (RegistryHealthStatus registryHealthStatus : registryHealthStatusList) {
                 Dimension dimension = registryHealthStatus.getDimension();
                 reportInfoStr.append(String
