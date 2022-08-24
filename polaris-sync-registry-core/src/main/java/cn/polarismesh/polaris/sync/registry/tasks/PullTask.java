44ad4bcce8dcd45d2e291bea5509aa2023e8ac90
liaochuntao <liaochuntao@live.com>
2022-08-24 10:13:48 +0800

    fix:添加错误日志信息打印

--- polaris-sync-registry-core/src/main/java/cn/polarismesh/polaris/sync/registry/tasks/PullTask.java
+++ polaris-sync-registry-core/src/main/java/cn/polarismesh/polaris/sync/registry/tasks/PullTask.java
@@ -86,7 +86,7 @@ public class PullTask implements Runnable {
                 }
             }
         } catch (Throwable e) {
-            LOG.error("[Core] pull task(source {}) encounter exception {}", source.getName(), e);
+            LOG.error("[Core] pull task(source {}) encounter exception {}", source.getName(), e.getStackTrace());
         }
     }
 
