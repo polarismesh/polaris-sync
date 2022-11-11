2f6e1c69a3a8633e8b29e35ab7ac537af4d44194
chuntaojun <liaochuntao@live.com>
2022-11-11 19:04:38 +0800

    feat:抽象数据拉取任务

---
+++ polaris-sync-core/src/main/java/cn/polarismesh/polaris/sync/core/tasks/Authorization.java
@@ -0,0 +1,14 @@
+package cn.polarismesh.polaris.sync.core.tasks;
+
+/**
+ * @author <a href="mailto:liaochuntao@live.com">liaochuntao</a>
+ */
+public class Authorization {
+
+	private String username;
+
+	private String password;
+
+	private String token;
+
+}
